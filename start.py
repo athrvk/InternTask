2ce6f7f27c14fa44db37d31570de69a9329c4a95
Atharva Kusumbia <45625606+athrvk@users.noreply.github.com>
2020-07-17 19:37:52 +0530

    Minor Modifications

--- start.py
+++ start.py
@@ -3,12 +3,13 @@ from city import *
 
 document = Document(filename="weather.xlsx")
 
-cities = [City("Indore", 1), City("Bhopal", 2), City('Noida', 3), City('Mumbai', 4), City('Delhi', 5)]
+cities = [City("Indore", 1), City("Bhopal", 2),
+          City('Noida', 3), City('Mumbai', 4),
+          City('Delhi', 5)]
 
 
 def start_the_program():
     initialize_document(document, cities)
-    write_citynames(document)  # in Sheet2
 
     while True:
         read_values_from_document(document, cities)
